c778fd9790a149864d84fce08d59439c60f254bc
vantadam <adem.zaier.9@gmail.com>
2024-01-02 13:59:18 +0100

    routes

--- Front/src/App.js
+++ Front/src/App.js
@@ -3,41 +3,47 @@ import { Home } from './component/home';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Navbar from './component/navbar';
 import SignInUpForm from './component/auth';
+import ClientHome from './component/clienthome';
+import Freehome from './component/freehome';
 
 function App() {
-  let IsAuthenticated = false;
-  useEffect(() => {
-    const fetchCurrentUser = async () => {
-      try {
-        const response = await fetch('http://localhost:8080/auth/currentUserId', {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}`,
-          },
-          method: 'GET',
-          mode: 'no-cors',
-        });
-        if (response.ok) {
-          localStorage.setItem('IsAuthenticated', 'true');
-        
-        }
-      } catch (error) {
-        console.error('Error fetching current user:', error);
-      }
-    };
+  
+  let Role = "freelancer";
 
-    fetchCurrentUser();
-  }, []);
-
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" Component={Home} />
-          <Route path="/auth" Component={SignInUpForm} />
-        </Routes>
-      </Router>
-    </div>
-  );
+  if (Role === "freelancer") {
+    return (
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/" Component={Home} />
+            <Route path="/auth" Component={SignInUpForm} />
+          </Routes>
+        </Router>
+      </div>
+    );
+  } else if (Role === "client") {
+    return (
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/" Component={ClientHome} />
+            <Route path="/auth" Component={SignInUpForm} />
+          </Routes>
+        </Router>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/" Component={Home} />
+            <Route path="/auth" Component={SignInUpForm} />
+          </Routes>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ Front/src/component/freehome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Freehome = () => {
+    return (
+        <div>
+            Freehome
+        </div>
+    );
+};
+
+export default Freehome;
